2ce1be485a7670d43b3523d88cb870701cffcc7e
Thomas Doig <thomasdoig@hotmail.com>
2021-02-02 14:06:20 +1000

    responsiveness for threejs

--- scripts/3dIndex.js
+++ scripts/3dIndex.js
@@ -1,18 +1,29 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://unpkg.com/three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'https://unpkg.com/three/examples/jsm/loaders/GLTFLoader.js'; 
 
 
-const loader = new GLTFLoader();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+scene.background = new THREE.Color( 0x131417)
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// For responsivenss with different screen sizes
+window.addEventListener('resize', function() 
+{
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  renderer.setSize( width, height );
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+});
+
 // const axesHelper = new THREE.AxesHelper( 5 );
 // scene.add( axesHelper );
 
-scene.background = new THREE.Color( 0x131417)
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+
 
 const ambient1 = new THREE.PointLight(0xffffff, 20);
 scene.add(ambient1);
@@ -22,11 +33,12 @@ const ambient2 = new THREE.AmbientLight(0xfffff, 5);
 scene.add(ambient2);
 ambient2.position.set(50, 5, 0);
 
-loader.load('js/scene.gltf', function(gltf) {
-  scene.add( gltf.scene );
-}, undefined, function(error) {
-    console.error( error );
-  });
+// const loader = new GLTFLoader();
+// loader.load('js/scene.gltf', function(gltf) {
+//   scene.add( gltf.scene );
+// }, undefined, function(error) {
+//     console.error( error );
+//   });
 
 const controls = new OrbitControls( camera, renderer.domElement);
 // camera.position.set( 0, 20, 100 );
